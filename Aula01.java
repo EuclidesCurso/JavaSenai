1b6f8a5c20b3cfff4e308bda57d0617717e30f34
EuclidesCurso <euclides.curso@gmail.com>
2024-07-24 22:36:49 -0300

    Int, String, Float, Double

---
+++ Aula01.java
@@ -0,0 +1,99 @@
+public class MyClass {
+  public static void main(String args[]) {
+        int a = 5;
+        int b = 2;
+        int resposta = a + b;
+
+     System.out.println(resposta);
+  }
+}
+
+//declarar int
+public class MyClass {
+  public static void main(String args[]) {
+        int a = 5;
+        int b = 2;
+        int resposta = a * b;
+
+     System.out.println(resposta);
+  }
+}
+
+
+//declarar String
+public class MyClass {
+  public static void main(String args[]) {
+   
+        String nome = "Euclides";
+        String sobreNome = "Junior";
+        String nomeCompleto = nome + " " + sobreNome;
+        
+        System.out.println(nomeCompleto);
+      
+    }
+}
+
+// declarar float
+public class MyClass {
+  public static void main(String args[]) {
+   
+    float qtdKg = (float) 0.5;
+    float precoKg = (float) 10.00;
+    
+    float total = qtdKg * precoKg;
+
+    System.out.println(total);
+  }
+}
+
+
+//declarar double e float juntos
+public class MyClass {
+  public static void main(String args[]) {
+   
+    double qtdKg = (float) 0.5;
+    float precoKg = (float) 10F;
+    
+    double total = qtdKg * precoKg;
+
+    System.out.println(total);
+  }
+}
+
+//tabuada do 7
+public class MyClass {
+  public static void main(String args[]) {
+    
+
+    System.out.println("Tabuada do 7 =" + "\n" + (7*1) + "\n" + (7*2) + "\n" + (7*3) + "\n" + (7*4) + "\n" + (7*5) + "\n" + (7*6) + "\n" + (7*7) + "\n" + (7*8) + "\n" + (7*9) + "\n" + (7*10) );
+  }
+}
+
+
+
+public class MyClass {
+  public static void main(String args[]) {
+        int a = 7;
+        int b = 1;
+        int c = 2;
+        int d = 3;
+        int e = 4;
+        int f = 5;
+        int g = 6;
+        int h = 7;
+        int i = 8;
+        int j = 9;
+        int k = 10;
+
+     System.out.println(a * b = );
+     System.out.println(a * c = );
+     System.out.println(a * d = );
+     System.out.println(a * e = );
+     System.out.println(a * f = );
+     System.out.println(a * g = );
+     System.out.println(a * h = );
+     System.out.println(a * i = );
+     System.out.println(a * j = );
+     System.out.println(a * k = );
+  }
+}
