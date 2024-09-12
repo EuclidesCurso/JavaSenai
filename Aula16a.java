82c923407f185924bde9aa9b99ce00c870eca069
EuclidesCurso <euclides.curso@gmail.com>
2024-09-11 22:37:17 -0300

    SobreCarga

---
+++ Aula16a.java
@@ -0,0 +1,16 @@
+
+public class Calculadora {
+	
+	public int soma(int a, int b) {
+		return a + b;
+	}
+	
+	public float soma(float a, float b) {
+		return a + b;
+	}
+	
+	public float soma(float a, float b, float c) {
+		return a + b + c;
+	}
+
+}
---
+++ Aula16b.java
@@ -0,0 +1,13 @@
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		Calculadora calc = new Calculadora();
+				
+		float resp = calc.soma(7.5F , 8.2F);
+		System.out.println(resp);
+
+	}
+
+}
